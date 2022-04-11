d6a9d40af656bf40d4f43136c5006e98d81f6099
mcmikey123 <77508686+mcmikey123@users.noreply.github.com>
2022-04-11 20:08:57 +0100

    More code cleanup

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -2,20 +2,20 @@ export const selectedProduct = (nr) =>{
     return{
         type:'selectedProduct',
         payload:nr
-    }
-}
+    };
+};
 
 export const defaultProduct = (nr) =>{
     return{
         type:'defaultProduct',
         payload:nr
-    }
-}
+    };
+};
 
 export const getBasketItems = (nr) =>{
     return{
         type:'getBasketItems',
         payload:nr
-    }
-}
+    };
+};
 
--- src/Api/Api.js
+++ src/Api/Api.js
@@ -4,6 +4,7 @@ const GetProducts = async () => {
     try {
         return await axios.get('Data/sampleData.json');
     } catch (error) {
+        // If we had a logging server, we'd also log this error
        return false;
     }
 }
